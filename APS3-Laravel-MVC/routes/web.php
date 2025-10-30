1201c553c8c1f6d3e85a06283d0da9b83fe40714
ArthurSousaDev <168488454+DevArthurSousa@users.noreply.github.com>
2025-10-30 19:26:25 +0000

    Versao final da atividade

---
+++ APS3-Laravel-MVC/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProdutoController;
+
+
+// MOSTRA a lista na página inicial
+Route::get('/', [CategoriaController::class, 'index']);
+
+// MOSTRA a lista também em /categorias (opcional, se você quiser)
+Route::get('/categorias', [CategoriaController::class, 'index']);
+
+// SALVA uma nova categoria quando o formulário é enviado
+Route::post('/categorias', [CategoriaController::class, 'store']);
+
+// Rotas de Produtos (exemplo)
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
