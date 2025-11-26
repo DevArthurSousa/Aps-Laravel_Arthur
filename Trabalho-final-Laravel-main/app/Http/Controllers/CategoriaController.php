2fffbfcb01d9954161e44f636e83abd4d2a1ff1e
ArthurSousaDev <168488454+DevArthurSousa@users.noreply.github.com>
2025-11-26 18:47:41 +0000

    commi trabalho final

--- Trabalho-final-Laravel-main/app/Http/Controllers/CategoriaController.php
+++ Trabalho-final-Laravel-main/app/Http/Controllers/CategoriaController.php
@@ -6,7 +6,7 @@ use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
-{
+{   
     public function index()
     {
         $categorias = Categoria::all();
